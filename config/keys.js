2c3b4a68a96451160e1f604c228936889a387d24
ram agrawal <34124239+minernova@users.noreply.github.com>
2023-05-26 14:41:05 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,1 @@
-module.exports = {
-  google: {
-    clientID:
-      "850958690747-53gqhvgdalrtmjlaa4b5l7uivlhfc095.apps.googleusercontent.com",
-    clientSecret: "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",
-  },
-  mongoose:{
-    dbURL:"mongodb://localhost/startersDB"
-  },
-  session:{
-    age:24*60*60*1000,
-    key:"@sfcaf6u4oiyvvds66x81ggg14470dsBL"
-  }
-};
+
